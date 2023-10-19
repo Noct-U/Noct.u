5cb0316a780d6e6bdef445d2ebf3589eeb43f4c3
PolianaMM <poliana.militao@sptech.school>
2023-10-19 19:13:43 -0300

    inicio do CRUD -  funcionario

--- site/public/js/cadastro-usuario.js
+++ site/public/js/cadastro-usuario.js
@@ -106,6 +106,117 @@ exibirLocatarias();
             });
     }
 
+    function publicar() {
+        var idUsuario = sessionStorage.ID_USUARIO;
+       
+        var corpo = {
+            nome: form_postagem.nome.value,
+            email: form_postagem.email.value,
+            senha: form_postagem.senha.value,
+            tipoFunc: form_postagem.tipoFunc.value,
+            empresa: form_postagem.empresa.value
+        }
+
+        fetch(`/avisos/publicar/${idUsuario}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(corpo)
+        }).then(function (resposta) {
+
+            console.log("resposta: ", resposta);
+
+            if (resposta.ok) {
+                window.alert("Post realizado com sucesso pelo usuario de ID: " + idUsuario + "!");
+                window.location = "/dashboard/listameta.html";
+                limparFormulario();
+                // finalizarAguardar();
+            } else if (resposta.status == 404) {
+                window.alert("Deu 404!");
+            } else {
+                throw ("Houve um erro ao tentar realizar a postagem! Código da resposta: " + resposta.status);
+            }
+        }).catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+            // finalizarAguardar();
+        });
+
+        return false;
+
+    }
+
+    function atualizarFeed() {
+        //aguardar();
+        var idUsuario = sessionStorage.ID_USUARIO;
+        
+        fetch(`/avisos/listar/${idUsuario}`).then(function (resposta) {
+
+            if (resposta.ok) {
+                if (resposta.status == 204) {
+                    var feed = document.getElementById("feed_container");
+                    var mensagem = document.createElement("span");
+                    mensagem.innerHTML = "Você ainda não possui funcionários cadastrados."
+                    feed.appendChild(mensagem);
+                    throw "Você ainda não possui funcionários cadastrados.";
+                }
+
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("feed_container");
+                    feed.innerHTML = "";
+                    for (let i = 0; i < resposta.length; i++) {
+                        var publicacao = resposta[i];
+
+                        // criando e manipulando elementos do HTML via JavaScript
+                        var divPublicacao = document.createElement("div");
+                        var spanID = document.createElement("span");
+                        var spanTitulo = document.createElement("span");
+                        var spanNome = document.createElement("span");
+                        var divDescricao = document.createElement("div");
+                        var divButtons = document.createElement("div");
+                        var btnEditar = document.createElement("button");
+                        var btnDeletar = document.createElement("button");
+
+                        // spanTitulo.innerHTML = "Título: <b>" + publicacao.titulo + "</b>";
+                        spanNome.innerHTML = "Leitor(a): <b>" + publicacao.nome + "</b>";
+                        divDescricao.innerHTML = publicacao.descricao + "</b>";
+                        btnDeletar.innerHTML = "Deletar";
+                        divPublicacao.className = "publicacao";
+                        spanTitulo.id = "inputNumero" + publicacao.idAviso;
+                        spanNome.className = "publicacao-nome";
+                        divDescricao.className = "publicacao-descricao";
+
+                        divButtons.className = "div-buttons"
+
+                        btnEditar.className = "publicacao-btn-editar"
+                        btnEditar.id = "btnEditar" + publicacao.idAviso;
+                        btnEditar.setAttribute("onclick", `editar(${publicacao.idAviso})`);
+
+                        btnDeletar.className = "publicacao-btn-editar"
+                        btnDeletar.id = "btnDeletar" + publicacao.idAviso;
+                        btnDeletar.setAttribute("onclick", `deletar(${publicacao.idAviso})`);
+
+                        divPublicacao.appendChild(spanID);
+                        divPublicacao.appendChild(spanNome);
+                        divPublicacao.appendChild(spanTitulo);
+                        divPublicacao.appendChild(divDescricao);
+                        divPublicacao.appendChild(divButtons);
+                        divButtons.appendChild(btnDeletar);
+                        feed.appendChild(divPublicacao);
+                    }
+
+                    // finalizarAguardar();
+                });
+            } else {
+                throw ('Houve um erro na API!');
+            }
+        }).catch(function (resposta) {
+            console.error(resposta);
+            // finalizarAguardar();
+        });
+    }
 
 
     
--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -6,7 +6,7 @@
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
+/*CREATE DATABASE aquatech;
 
 USE aquatech;
 
@@ -99,12 +99,163 @@ CREATE TABLE medida (
 	chave TINYINT,
 	momento DATETIME,
 	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
+);*/
 
 /*
 comandos para criar usuário em banco de dados azure, sqlserver,
 com permissão de insert + update + delete + select
 */
+
+CREATE DATABASE noctuBD;
+USE noctuBD;
+
+CREATE TABLE empresa(
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    razaoSocial VARCHAR(100) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    telefoneFixo CHAR(12) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(15) NOT NULL
+);
+
+INSERT INTO empresa(nome, razaoSocial, cnpj, telefoneFixo, email, senha) VALUES
+	('Simpress', 'Ltda', '12356789019283', '119333576377', 'simpress@gmail.com', 'admin123');
+
+CREATE TABLE endereco(
+	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+	cep CHAR(8) NOT NULL,
+    uf CHAR(2) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    bairro VARCHAR(45) NOT NULL,
+    logradouro VARCHAR(45) NOT NULL
+);
+
+INSERT INTO endereco (cep, uf, cidade, bairro, logradouro) VALUES
+	('08474230', 'SP', 'São Paulo', 'Paulista', 'Rua Haddock Lobo');
+
+CREATE TABLE local(
+	idLocal INT AUTO_INCREMENT,
+    numero INT NOT NULL,
+    complemento VARCHAR(45),
+    andar INT,
+    sala VARCHAR(45),
+    fkEndereco INT NOT NULL,
+    fkEmpresa INT NOT NULL,
+    FOREIGN KEY (fkEndereco) REFERENCES endereco(idEndereco),
+    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
+    PRIMARY KEY (idLocal, fkEndereco, fkEmpresa)
+);
+
+INSERT INTO local (numero, andar, sala, fkEndereco, fkEmpresa) VALUES
+	(211, 7, '7A', 1, 1);
+
+CREATE TABLE empresaLocataria (
+	idEmpresaLocataria INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    cnpj CHAR(14),
+    fkMatriz INT,
+    fkEmpresa INT,
+    FOREIGN KEY (fkMatriz) REFERENCES empresaLocataria(idEmpresaLocataria),
+    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+INSERT INTO empresaLocataria (nome, cnpj, fkEmpresa) VALUES
+	('SPTech', '10293029381203', 1);
+    
+CREATE TABLE tipoUsuario(
+	idTipoUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nomeTipo VARCHAR(45) NOT NULL
+);
+
+INSERT INTO tipoUsuario (nomeTipo) VALUES
+	('Admin'),
+	('Funcionario');
+    
+CREATE TABLE usuario(
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+	email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL,
+    fkTipoUsuario INT NOT NULL,
+    fkEmpresaAlocacao INT NOT NULL,
+    FOREIGN KEY (fkEmpresaAlocacao) REFERENCES empresaLocataria(idEmpresaLocataria),
+    FOREIGN KEY (fkTipoUsuario) REFERENCES tipoUsuario (idTipoUsuario)
+);
+
+INSERT INTO  usuario (nome, email, senha, fkTipoUsuario, fkEmpresaAlocacao) VALUES
+	('Kevin', 'kevin.rsilva07@gmail.com', '1234568', 2, 1);
+
+CREATE TABLE modeloComputador(
+	idModeloComputador INT PRIMARY KEY auto_increment,
+    nome VARCHAR(45)
+);
+
+INSERT INTO modeloComputador (nome) VALUES
+	('IdeaPad');
+
+CREATE TABLE computador(
+	idComputador INT PRIMARY KEY auto_increment,
+    numeroSerie VARCHAR(45),
+    fkEmpresa INT,
+    fkModeloComputador INT,
+    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
+    FOREIGN KEY (fkModeloComputador) REFERENCES modeloComputador(idModeloComputador)
+);
+
+INSERT INTO computador (numeroSerie, fkEmpresa, fkModeloComputador) VALUES
+	('2213131xy321', 1, 1);
+
+CREATE TABLE tipoHardware(
+	idTipoHardware INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL
+);
+
+INSERT INTO tipoHardware (nome) VALUES
+	('CPU'),
+	('RAM'),
+	('Disco'),
+	('Janelas');
+
+CREATE TABLE hardware(
+	idHardware INT PRIMARY KEY auto_increment,
+    nome VARCHAR(100) NOT NULL,
+    especificidade VARCHAR(45),
+    capacidade FLOAT NOT NULL,
+	fkTipoHardware INT,
+    FOREIGN KEY (fkTipoHardware) REFERENCES tipoHardware(idTipoHardware)
+);
+-- JAR pega
+
+CREATE TABLE componente(
+	idComponente INT AUTO_INCREMENT,
+    fkHardware INT,
+    fkComputador INT,
+    codigoSerial VARCHAR(45),
+    FOREIGN KEY (fkHardware) REFERENCES hardware(idHardware),
+    FOREIGN KEY (fkComputador) REFERENCES computador(idComputador),
+    PRIMARY KEY (idComponente, fkHardware, fkComputador)
+);
+-- JAR pega
+
+CREATE TABLE captura (
+	idCaptura INT PRIMARY KEY AUTO_INCREMENT,
+    valor FLOAT,
+    descricao VARCHAR(45),
+    dtCaptura DATETIME,
+    fkComputador INT,
+    fkHardware INT,
+    fkComponente INT,
+    FOREIGN KEY (fkComputador) REFERENCES componente(fkComputador),
+    FOREIGN KEY (fkHardware) REFERENCES componente(fkHardware),
+    FOREIGN KEY (fkComponente) REFERENCES componente(idComponente)
+);
+
+-- JAR pega
+
+select * from hardware;
+select * from componente;
+select * from captura;
 
 CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
 WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
